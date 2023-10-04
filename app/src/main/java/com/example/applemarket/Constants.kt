a69114411ec1538af4f956c24248d533d4a4368c
Lee do yeon <simon3397@gmail.com>
2023-10-04 17:57:58 +0900

    [INIT] 값, 상수 정의 리소스 파일 생성

---
+++ app/src/main/java/com/example/applemarket/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.applemarket
+//앱 전체에서 사용되는 상수 정의 파일
+object Constants {
+    // ITEM_INDEX 와 ITEM_OBJECT 상수는 액티비티 간 데이터 전달 시 사용
+    // 액티비티 간 데이터 전달하거나 구분하기 위한 키 역할
+    // 코드 가동성 향상 및 상수 값 중복 사용 방지
+    const val ITEM_INDEX = "item_index"
+    const val ITEM_OBJECT = "item_object"
+}
